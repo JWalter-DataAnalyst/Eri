fa5774290bd32d40e8f2d6c07befa40066189698
eringogel <eri.n.gogel@gmail.com>
2026-03-28 02:25:23 +0000

    Deploying to gh-pages from @ eringogel/Eri@cc1461ed151dfab19e7241b6e96b902ac76d172a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "JWalter-DataAnalyst/Eri",
+    "data-repo": "eringogel/Eri",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
